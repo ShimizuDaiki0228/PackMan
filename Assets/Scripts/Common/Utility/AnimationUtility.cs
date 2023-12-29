48f77631546c1dd270b0df95ef5b591db0a57c9b
ShimizuDaiki0228 <kis1181100133@gmail.com>
2023-12-29 14:36:37 +0900

    TextMeshPro追加

---
+++ Assets/Scripts/Common/Utility/AnimationUtility.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public static class AnimationUtility
+{
+    /// <summary>
+    /// テキストをスライドで表示するアニメーション
+    /// </summary>
+    /// <param name="firstPosition">スタート位置</param>
+    /// <param name="destinationPosition">最終表示位置</param>
+    /// <param name="duration">アニメーション時間</param>
+    /// <returns></returns>
+    //public static DOTween TextSlideAnimation(Text text, Vector3 firstPosition, Vector3 destinationPosition, float duration)
+    //{
+    //    return text.Do
+    //}
+}
